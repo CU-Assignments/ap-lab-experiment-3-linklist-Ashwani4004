1a5b9783d14956754e0c0fa04fe0cab31f573f7b
Ashwani 22BCS10545 <118659914+Ashwani4004@users.noreply.github.com>
2025-02-17 13:48:48 +0530

    Add files via upload

---
+++ MergeTwoSortedLists.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    public:
+        ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+            if (!list1) return list2;
+            if (!list2) return list1;
+            
+            if (list1->val < list2->val) {
+                list1->next = mergeTwoLists(list1->next, list2);
+                return list1;
+            } else {
+                list2->next = mergeTwoLists(list1, list2->next);
+                return list2;
+            }
+        }
+    };
+    
+    void printList(ListNode* head) {
+        while (head) {
+            cout << head->val << " -> ";
+            head = head->next;
+        }
+        cout << "NULL\n";
+    }
